54c9b5d7e0eb5c0cfd17e4042abcea95689c4464
Kadir korkmaz <kadir@Kadirs-MacBook-Pro.local>
2016-07-17 12:08:33 +0300

    Default user registration machanism added. Default user properties inserted into application.properties Log4J2 dependency added.

--- src/main/java/com/kadirkorkmaz/Application.java
+++ src/main/java/com/kadirkorkmaz/Application.java
@@ -1,22 +1,12 @@
 package com.kadirkorkmaz;
 
-import com.kadirkorkmaz.database.repository.MediaReository;
-import com.kadirkorkmaz.database.repository.NavigationRepository;
-import com.kadirkorkmaz.database.repository.UserRepository;
 import org.pegdown.PegDownProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
-    @Autowired
-    UserRepository userRepo;
-
-    @Autowired
-    MediaReository mediaRepo;
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
@@ -30,24 +20,6 @@ public class Application {
     @Bean
     public PegDownProcessor getPegDownProcessor() {
         return new PegDownProcessor();
-    }
-
-    @Autowired
-    NavigationRepository navRepo;
-
-    @Bean
-    public Integer recordImageContent() {
-
-//        String[] images_ = {"kadirkorkmaz.jpg", "castle.jpg", "bridge.jpg"};
-//        List<String> images = new LinkedList<>();
-//        Random generator = new Random();
-//        for (int i = 1; i < 25; i++) {
-//            int randomNumber = generator.nextInt(3);
-//            MediaEntity m = new MediaEntity(MediaTypeEnum.IMAGE, images_[randomNumber], "Only Image", (String.valueOf(randomNumber) + ".jpg"));
-//            mediaRepo.save(m);
-//        }
-        
-        return 0;
     }
 
 }
---
+++ src/main/java/com/kadirkorkmaz/configuration/DefaultUserConfiguration.java
@@ -0,0 +1,59 @@
+package com.kadirkorkmaz.configuration;
+
+import com.kadirkorkmaz.database.entity.UserEntity;
+import com.kadirkorkmaz.database.entity.UserRole;
+import com.kadirkorkmaz.database.entity.UserRoleEnum;
+import com.kadirkorkmaz.database.repository.UserRepository;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author kadir
+ */
+@Configuration
+public class DefaultUserConfiguration {
+
+    private static final Logger logger = Logger.getLogger(DefaultUserConfiguration.class);
+
+    @Autowired
+    UserRepository userRepo;
+
+    @Value("${defaut.user.name}")
+    String name;
+    @Value("${defaut.user.surname}")
+    String surname;
+    @Value("${defaut.user.username}")
+    String userName;
+    @Value("${defaut.user.password}")
+    String password;
+
+    @Bean
+    public UserEntity registerDefaultUser() {
+
+        UserEntity defaultUser = userRepo.findOneByUserName(userName);
+
+        if (defaultUser != null) {
+            logger.info("Default user is alreadey registered.");
+            return defaultUser;
+        }
+
+        logger.info("Creating default user.");
+        
+        defaultUser = new UserEntity(name, surname, userName, password);
+
+        defaultUser.addToRoles(new UserRole(UserRoleEnum.ADMIN));
+        defaultUser.addToRoles(new UserRole(UserRoleEnum.WRITER));
+        
+        logger.info("Seaving default user.");
+        
+        userRepo.save(defaultUser);
+
+        return defaultUser;
+    }
+}
